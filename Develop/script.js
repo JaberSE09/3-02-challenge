a1fcb40bfb8e496bd1fcbfa71af0ef8a84176da1
Sajid Jaber <sajidjaber@yahoo.com>
2022-03-06 17:04:39 -0600

    setup the js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,38 @@
 // Assignment code here
+function  generatePassword(){
+  var numbers = "0123456789";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var symbols = "!@#$%^&*()";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lengthpass = "";
+  var criteria="";
+  var pass = "";
+
+  if(type == "upppercase"){
+
+    criteria += upperCase;
+  }
+  if(type == "lowercase"){
+
+    criteria += lowerCase;
+  }
+  if(type == "symbols"){
+
+    criteria += symbols;
+  }
+  if(type == "numberic"){
+
+    criteria += numbers;
+  }
+  return pass;
+}
+
+function alert(){
+alert
 
 
+
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,8 +40,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+""
+  passwordText.innerHTML = password;
 
 }
 
